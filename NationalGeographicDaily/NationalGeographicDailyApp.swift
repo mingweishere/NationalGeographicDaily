1022a2084192b5188f300c4201a592a260e6b118
Prophetjames <mingweishere@gmail.com>
2026-05-15 09:41:03 -0700

    Initial Commit

---
+++ NationalGeographicDaily/NationalGeographicDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NationalGeographicDailyApp.swift
+//  NationalGeographicDaily
+//
+//  Created by Mingwei Wei on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct NationalGeographicDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
